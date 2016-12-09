0477d99c590b2b7102dd9f20636990bd612a5cf7
soapinduty <g4mrator@gmail.com>
2016-12-09 04:24:34 +0100

    MainMenu added.

---
+++ Assets/Scripts/MovementMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    transform.Rotate(0, 20 * Time.deltaTime, 0);
+
+    }
+}
