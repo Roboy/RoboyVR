7d81c5419854a10300c75fe40ccad3fdf5e21fa5
Egor Shevdin <egor_sh@live.de>
2017-02-06 21:39:51 +0100

    Graphrenderer improved

---
+++ Assets/Editor/GraphRendererEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(GraphRenderer))]
+public class GraphRendererEditor : Editor
+{
+
+
+    GraphRenderer m_Instance;
+    PropertyField[] m_fields;
+
+
+    public void OnEnable()
+    {
+        m_Instance = target as GraphRenderer;
+        m_fields = ExposeProperties.GetProperties(m_Instance);
+    }
+
+    public override void OnInspectorGUI()
+    {
+
+        if (m_Instance == null)
+            return;
+
+        this.DrawDefaultInspector();
+
+        ExposeProperties.Expose(m_fields);
+
+    }
+}
--- Assets/Scripts/GraphRenderer.cs
+++ Assets/Scripts/GraphRenderer.cs
@@ -3,18 +3,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 [RequireComponent(typeof(Image))]
+
 public class GraphRenderer : MonoBehaviour
 {
     // Color for the graph
     public Material GraphMaterial;
 
-    // Offset values
+    // Offset values    
     public float BorderLeft;
     public float BorderRight;
     public float BorderTop;
     public float BorderBottom;
+
+    [ExposeProperty]
+    public bool ShowCurrentValue
+    {
+        get { return m_ShowCurrentValue; }
+        set
+        {
+            if (m_ShowCurrentValue != value)
+            {
+                m_ShowCurrentValue = value;
+
+                if (m_ShowCurrentValue)
+                    createTextfieldForCurrentValue();
+                else
+                    destroyTextfieldForCurrentValue();
+            }
+        }
+    }
+    [HideInInspector]
+    [SerializeField]
+    private bool m_ShowCurrentValue = false;
+
+    [HideInInspector]
+    [SerializeField]
+    private GameObject m_TextForCurrentValue;
+
+    // We make the border offsets read only in play mode so we need extra variables so we can reset them on change in play mode in OnValidate
+    private float m_BorderLeftStatic;
+    private float m_BorderRightStatic;
+    private float m_BorderTopStatic;
+    private float m_BorderBottomStatic;
+
+    // We set the static offset variables to the inital values in Awake and mark the offsets as initialized
+    private bool m_BorderInitialized;
 
     // LineRenderer to render the graph
     private LineRenderer m_OscillatorLineRenderer;
@@ -65,6 +103,88 @@ public class GraphRenderer : MonoBehaviour
         Stop();
     }
 
+    // In the unity editor we make sure that the values do not make up a negative area and make them read only in play mode
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (!EditorApplication.isPlaying)
+        {
+            // Make sure that the inner part is at least 10% of the panel
+            RectTransform rectTransform = GetComponent<RectTransform>();
+
+            BorderLeft = Mathf.Clamp(BorderLeft, 0f, rectTransform.rect.width - BorderRight - 0.1f * rectTransform.rect.width);
+            BorderRight = Mathf.Clamp(BorderRight, 0f, rectTransform.rect.width - BorderLeft - 0.1f * rectTransform.rect.width);
+            BorderBottom = Mathf.Clamp(BorderBottom, 0f, rectTransform.rect.height - BorderTop - 0.1f * rectTransform.rect.height);
+            BorderTop = Mathf.Clamp(BorderTop, 0f, rectTransform.rect.height - BorderBottom - 0.1f * rectTransform.rect.height);
+        }
+        else
+        {
+            // In play mode reset the values when they are changed
+            if (m_BorderInitialized)
+            {
+                BorderLeft = m_BorderLeftStatic;
+                BorderRight = m_BorderRightStatic;
+                BorderTop = m_BorderTopStatic;
+                BorderBottom = m_BorderBottomStatic;
+            }
+        }
+    }
+
+
+#endif
+
+    /// <summary>
+    /// Draw a rectangle to display the bordered panel where the graph will be rendered
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        // Get the rect transform component for later use
+        RectTransform rectTransform = GetComponent<RectTransform>();
+
+        // calculate the center of the cube
+        Vector2 pivotDifference = new Vector2(0.5f - rectTransform.pivot.x, 0.5f - rectTransform.pivot.y);
+
+        float maximumWidth = transform.lossyScale.x * (rectTransform.rect.width);
+        float maximumHeight = transform.lossyScale.y * (rectTransform.rect.height);
+
+        float borderLeftScaled = BorderLeft / rectTransform.rect.width;
+        float borderRightScaled = BorderRight / rectTransform.rect.width;
+        float borderTopScaled = BorderTop / rectTransform.rect.height;
+        float borderBottomScaled = BorderBottom / rectTransform.rect.height;
+
+        // Get the center to be the real center of the panel independent of the pivot point
+        Vector3 centerOfWholeImage = new Vector3(pivotDifference.x * maximumWidth / 2f, pivotDifference.y * maximumHeight * 2f, 0f);
+
+        // Get the center of the panel with borders
+        Vector3 centerOfBorderedImage = new Vector3(centerOfWholeImage.x - ((borderRightScaled - borderLeftScaled) * 0.5f), centerOfWholeImage.y - ((borderTopScaled - borderBottomScaled) * 0.5f), 0f);
+
+        // Calculate the cube to display the offset by the borders
+        Vector3 panelSize = new Vector3(maximumWidth, maximumHeight, 0f);
+
+        // As we apply a transform matrix with panel size as scale vector we need to subtract the size of the bordered panel from a (1,1,1) vector
+        Vector3 panelBorderedSize = Vector3.one - new Vector3(borderLeftScaled + borderRightScaled, borderTopScaled + borderBottomScaled, 0f);
+
+        // Set the gizmos matrix to match the right transform values
+        Matrix4x4 oldGizmosMatrix = Gizmos.matrix;
+        Matrix4x4 transformMatrix = Matrix4x4.TRS(transform.position, transform.rotation, panelSize);
+        Gizmos.matrix = transformMatrix;
+
+        // Finally draw the wire cube
+        Gizmos.DrawWireCube(centerOfWholeImage + (centerOfBorderedImage - centerOfWholeImage), panelBorderedSize);
+
+        Gizmos.matrix = oldGizmosMatrix;
+    }
+
+    private void Awake()
+    {
+        // Intialize the static border offsets so we can reset the border values in OnValidate
+        m_BorderBottomStatic = BorderBottom;
+        m_BorderTopStatic = BorderTop;
+        m_BorderLeftStatic = BorderLeft;
+        m_BorderRightStatic = BorderRight;
+        m_BorderInitialized = true;
+    }
+
     /// <summary>
     /// Initialize all the internal parameters 
     /// </summary>
@@ -78,7 +198,7 @@ public class GraphRenderer : MonoBehaviour
             // Intialize the values list with values from the given list or set them to zero if numPoints exceeds the list length
             for (int i = 0; i < numPoints; i++)
             {
-                if(i < valueList.Count)
+                if (i < valueList.Count)
                     m_CurrentValues.Add(valueList[i]);
                 else
                     m_CurrentValues.Add(0f);
@@ -88,12 +208,12 @@ public class GraphRenderer : MonoBehaviour
             m_RectTransform = GetComponent<RectTransform>();
 
             // Get the maximum scaled width and height
-            m_MaximumWidth = (m_RectTransform.rect.width - BorderLeft - BorderRight) * transform.lossyScale.x;
-            m_MaximumHeight = (m_RectTransform.rect.height - BorderBottom - BorderTop) * transform.lossyScale.y;
-            
+            m_MaximumWidth = (m_RectTransform.rect.width - BorderLeft - BorderRight);
+            m_MaximumHeight = (m_RectTransform.rect.height - BorderBottom - BorderTop);
+
             // Number of points of the graph
             m_NumPoints = numPoints;
-            m_StepSize = m_MaximumWidth / ((float)numPoints-1);
+            m_StepSize = m_MaximumWidth / ((float)numPoints - 1);
 
             m_OriginValues = valueList;
             m_TimeStep = timeStep;
@@ -120,6 +240,7 @@ public class GraphRenderer : MonoBehaviour
         {
             // Create a LineRenderer component and attach it with the given parameters
             m_OscillatorLineRenderer = gameObject.AddComponent<LineRenderer>();
+            m_OscillatorLineRenderer.useWorldSpace = false;
             m_OscillatorLineRenderer.numPositions = m_NumPoints;
             m_OscillatorLineRenderer.material = GraphMaterial;
             m_OscillatorLineRenderer.startWidth = m_OscillatorLineRenderer.endWidth = 0.01f;
@@ -128,15 +249,16 @@ public class GraphRenderer : MonoBehaviour
             // Update graph position each frame
             m_PlayCoroutine = playCoroutine();
             // Update graph values each timestep
-            m_UpdateValuesCoroutine = updateValuesCoroutine();           
+            m_UpdateValuesCoroutine = updateValuesCoroutine();
             m_CurrentState = State.Playing;
             StartCoroutine(m_PlayCoroutine);
             StartCoroutine(m_UpdateValuesCoroutine);
         }
         else if (m_CurrentState == State.Paused)
         {
-            StartCoroutine(m_UpdateValuesCoroutine);
             m_CurrentState = State.Playing;
+            m_UpdateValuesCoroutine = updateValuesCoroutine();
+            StartCoroutine(m_UpdateValuesCoroutine);
         }
         else if (m_CurrentState == State.None)
         {
@@ -153,6 +275,7 @@ public class GraphRenderer : MonoBehaviour
         {
             // Stop updating the values
             StopCoroutine(m_UpdateValuesCoroutine);
+            m_UpdateValuesCoroutine = null;
             m_CurrentState = State.Paused;
         }
     }
@@ -164,8 +287,12 @@ public class GraphRenderer : MonoBehaviour
         if (m_CurrentState != State.None)
         {
             // Kill linerenderer, stop the coroutines
-            StopCoroutine(m_PlayCoroutine);
-            StopCoroutine(m_UpdateValuesCoroutine);
+            if (m_PlayCoroutine != null)
+                StopCoroutine(m_PlayCoroutine);
+            if (m_UpdateValuesCoroutine != null)
+                StopCoroutine(m_UpdateValuesCoroutine);
+            m_UpdateValuesCoroutine = null;
+            m_PlayCoroutine = null;
             Destroy(m_OscillatorLineRenderer);
             m_CurrentState = State.None;
         }
@@ -224,11 +351,21 @@ public class GraphRenderer : MonoBehaviour
                 else
                     m_CurrentValues.Add(0f);
             }
+
+            if (m_ShowCurrentValue && m_TextForCurrentValue)
+            {
+                m_TextForCurrentValue.GetComponent<Text>().text = m_CurrentValues[0].ToString();
+            }
             yield return new WaitForSeconds(m_TimeStep);
+        }
+
+        if (m_CurrentState != State.Playing)
+        {
+            Debug.Log("Graph is not playing! Cannot update the values!");
         }
     }
 
-    float findSmallestElement(List<float> list )
+    float findSmallestElement(List<float> list)
     {
         if (list == null)
         {
@@ -289,7 +426,7 @@ public class GraphRenderer : MonoBehaviour
     {
         if (min == max)
             return value;
-        return (value - min)*m_MaximumHeight/(max - min);
+        return (value - min) * m_MaximumHeight / (max - min);
     }
     /// <summary>
     /// Transforms point from local space to panel space
@@ -298,13 +435,29 @@ public class GraphRenderer : MonoBehaviour
     /// <returns></returns>
     Vector3 getGraphPositionAtIndex(int index)
     {
-        Vector3 pos = new Vector3(index * m_StepSize, scaleValues(m_MinValue, m_MaxValue, m_CurrentValues[index]), -0.025f);
-        pos = transform.rotation * pos;
-        pos += transform.position;
-        pos -= m_RectTransform.pivot.x * m_MaximumWidth * transform.right;
-        pos -= m_RectTransform.pivot.y * m_MaximumHeight * transform.up;
+        Vector3 pos = new Vector3(index * m_StepSize, scaleValues(m_MinValue, m_MaxValue, m_CurrentValues[index]), -5f);
+        pos -= m_RectTransform.pivot.x * m_MaximumWidth * Vector3.right;
+        pos -= m_RectTransform.pivot.y * m_MaximumHeight * Vector3.up;
+        Vector2 pivotDifference = Vector2.one * 0.5f - m_RectTransform.pivot;
+
+        pos += new Vector3(m_MaximumWidth * pivotDifference.x + ((BorderLeft - BorderRight) * 0.5f), m_MaximumHeight * pivotDifference.y + ((BorderBottom - BorderTop) * 0.5f), 0f);
         return pos;
     }
 
-    
+    private void createTextfieldForCurrentValue()
+    {
+        m_TextForCurrentValue = new GameObject();
+        m_TextForCurrentValue.name = "Current Value";
+        m_TextForCurrentValue.transform.parent = transform;
+        m_TextForCurrentValue.transform.localScale = transform.localScale;
+
+        m_TextForCurrentValue.AddComponent<Text>();
+    }
+
+    private void destroyTextfieldForCurrentValue()
+    {
+        DestroyImmediate(m_TextForCurrentValue);
+    }
+
+
 }
