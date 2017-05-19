728ff127dd70c9f9aba50834354740f93a288f95
missxa <aliona.kharchenko@gmail.com>
2017-05-19 17:50:51 +0200

    RoboyManager now has MeshUpdater Script

---
+++ Assets/Editor/MeshUpdaterEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MeshUpdater))]
+class MeshUpdaterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        //GUILayout.TextField("text");
+        if (GUILayout.Button("Run"))
+        {
+            MeshUpdater a = new MeshUpdater();
+            a.MeshUpdate(a.pathToScript, a.pathToBlender+a.pathToMeshes);
+        }
+            
+    }
+}
---
+++ Assets/Scripts/MeshUpdater.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Diagnostics;
+
+
+public class MeshUpdater : MonoBehaviour {
+
+
+    //"pathToBlender/blender.exe"
+    public string pathToBlender = "C:\\Program Files\\Blender Foundation\\Blender\\blender.exe";
+
+    //"pathToScript\meshDownloadScript.py"
+    public string pathToScript = "D:\\Unity Projects\\RoboyVR\\Assets\\RoboyModel\\OriginModels\\meshDownloadScript.py";
+
+    //"gitPathToMeshes\" 
+    public string pathToMeshes = "https://github.com/Roboy/roboy_models/tree/master/legs_with_upper_body/cad";
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MeshUpdate(string cmd, string args) {
+        UnityEngine.Debug.Log("I built this with my bear hands!");
+        UnityEngine.Debug.Log(pathToBlender);
+        UnityEngine.Debug.Log(pathToScript);
+        UnityEngine.Debug.Log(pathToMeshes);
+        //ProcessStartInfo start = new ProcessStartInfo();
+        //start.FileName = pathToScript;
+        //start.Arguments = string.Format("{0} {1}", cmd, args);
+        //start.UseShellExecute = false;
+        //start.RedirectStandardOutput = true;
+        //using (Process process = Process.Start(start))
+        //{
+        //    using (StreamReader reader = process.StandardOutput)
+        //    {
+        //        string result = reader.ReadToEnd();
+        //        Console.Write(result);
+        //    }
+        //}
+    }
+}
