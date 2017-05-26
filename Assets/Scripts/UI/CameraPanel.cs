3d65bcfe049ed08b01ee77b8ad60a1153a6b50f9
missxa <aliona.kharchenko@gmail.com>
2017-05-26 16:07:43 +0200

    Added functionality to change the size of the camera image.

---
+++ Assets/Scripts/UI/CameraPanel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraPanel : MonoBehaviour {
+
+    /// <summary>
+    /// Image which size that is to be modified, wrapped in a RectTransform..
+    /// </summary>
+    public RectTransform Rt;
+    public Slider Sl;
+
+    [SerializeField]
+    private float m_AspectRatio;
+    private float m_Scale = 1.0f;
+    private float m_Width_Ref;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+    m_Width_Ref = Rt.rect.width;
+    m_AspectRatio = Rt.rect.width / Rt.rect.height;
+}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Sl.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+
+    }
+
+    private void ValueChangeCheck()
+    {
+        float widthTmp = Rt.rect.width;
+        float heightTmp = Rt.rect.height;
+        float scaleDiff = 0.0f;
+
+
+        if (m_Scale < Sl.value)
+        {
+            scaleDiff = Sl.value - m_Scale;
+            Rt.sizeDelta = new Vector2(m_Width_Ref + widthTmp*scaleDiff, (m_Width_Ref + widthTmp * scaleDiff) / m_AspectRatio );
+            
+        }
+        if (m_Scale > Sl.value)
+        {
+            scaleDiff = m_Scale - Sl.value;
+            Rt.sizeDelta = new Vector2(m_Width_Ref - widthTmp * scaleDiff, (m_Width_Ref - widthTmp * scaleDiff) / m_AspectRatio);
+            
+        }
+
+    }
+}
