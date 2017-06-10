a4a5e54d9636d124ed193c3646e1fd2113e2516d
Joana Pecholt <jopech@gmx.de>
2017-06-11 01:07:35 +0200

    More scripts and small adjustments

---
+++ Assets/Scripts/UI/DisplayManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Handles multiple displays.
+/// Upon start, 2 displays are activated if 2 cameras and 2 monitors are found. 
+/// Additional information is shortly displayed on the main screen.
+/// </summary>
+public class DisplayManager : MonoBehaviour
+{
+    public Camera cam2; /*This camera will be used for the second display*/
+    /// <summary>
+    /// Activates 2 Displays if multiple detected. Displays short info about display number on Main screen.
+    /// </summary>
+    void Start()
+    {
+        Canvas c = CreateInfoScreen();
+        Text t = c.GetComponentInChildren<Text>();
+        Debug.Log(Display.displays.Length + " Displays found... activating");
+        t.text = Display.displays.Length + " Displays found... activating";
+        StartCoroutine(DisplayFor(c, 1.5f));
+        // Display.displays[0] is the primary, default display and is always ON.
+        // Check if additional displays are available and activate each.
+        if (Display.displays.Length > 1 && (cam2!=null)) { 
+            //TODO: windowed mode supposed to work under windows... might blow up under Linux / OSX !!
+            Display.displays[1].Activate(400, 400, 60);
+            cam2.GetComponent<Camera>().targetDisplay = 1;
+        }
+    }
+
+    /// <summary>
+    /// Create Text on Canvas to be displayed in centre of screen.
+    /// </summary>
+    /// <returns>Canvas containing text object</returns>
+    Canvas CreateInfoScreen()
+    {
+        GameObject g = new GameObject();
+        g.transform.parent = transform;
+        Canvas c = g.AddComponent<Canvas>();
+        c.targetDisplay =0;
+        c.transform.position = Vector3.zero;
+        c.renderMode = RenderMode.ScreenSpaceOverlay;
+        CanvasScaler cs = g.AddComponent<CanvasScaler>();
+        cs.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+        cs.scaleFactor = 1;
+        cs.referencePixelsPerUnit = 100;
+        GraphicRaycaster rc = g.AddComponent<GraphicRaycaster>();
+        rc.ignoreReversedGraphics = true;
+        g.transform.localScale = new Vector3(1, 1, 1);
+        g.transform.localPosition = Vector3.zero;
+        GameObject g2 = new GameObject();
+        Text t = g2.AddComponent<Text>(); /*Text message displaying information about screens*/
+        g2.transform.SetParent(g.transform, false);
+        t.horizontalOverflow = HorizontalWrapMode.Overflow;
+        t.verticalOverflow = VerticalWrapMode.Overflow;
+        t.alignment = TextAnchor.MiddleLeft;
+        Font f = (Font) Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+        t.font = f;
+        t.fontSize = 30;
+        t.color = Color.black;
+        t.enabled = true;
+        return c;
+    }
+
+    /// <summary>
+    /// Displays specified canvas before it is destroyed.
+    /// </summary>
+    /// <param name="c">Canvas to be displayed</param>
+    /// <param name="sec">Time to display canvas</param>
+    /// <returns></returns>
+    IEnumerator DisplayFor( Canvas c, float sec)
+    {
+        if (c == null) yield return null;
+        c.enabled = true;
+        yield return new WaitForSeconds(sec);
+        foreach (Component comp in c.GetComponentsInChildren<Component>())
+        {
+            Destroy(comp.gameObject);            
+        }
+    }
+}
---
+++ Assets/Scripts/UI/SelectionWheelManager.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionWheelManager : MonoBehaviour
+{
+    private bool visible = false; /* Is selection wheel visible */
+    private bool stillVisible = false; /* before disabling wheel:was it moved again? Should it still be visible*/
+    private double curSpin = 0; /* rotational speed, changed and updated over time*/
+    private float curAngle = 0;/* current angle around z axis (local)*/
+    private int elems; /* number of text elems in selection wheel*/
+    private int selected = -1; /* which text elem is selected*/
+    public float speed; /* factor to multiply curSpin with, allows manual settings*/
+    public float threshold; /* Spin below that point set 0*/
+    public int selectIndex = 1; /* Specify where selected item should be positioned (clockwise, number of elem on circle)*/
+    public Canvas canvas; /* canvas with selection wheel, en- and disabled depending on wheel */
+    
+    #region helpers
+    /// <summary>
+    /// x mod y in a function, as normal % just returns remainder and does not work for negative values.
+    /// </summary>
+    /// <param name="x">first operand</param>
+    /// <param name="y">second operand</param>
+    /// <returns></returns>
+    int Mod(int x, int y)
+    {
+        return (x % y + y) % y;
+    }
+
+    /// <summary>
+    /// Enables canvas containing wheel to be displayed
+    /// </summary>
+    void EnableCanvas()
+    {
+        if (canvas) { 
+            canvas.GetComponent<Canvas>().enabled = true;
+            visible = true;
+        }
+    }
+    /// <summary>
+    /// Coroutine to wait shortly (0.5s) and disable canvas if user did not give new input meanwhile.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator DisableCanvas()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (!stillVisible)
+        {
+            if (canvas)
+            {
+                canvas.GetComponent<Canvas>().enabled = false;
+                visible = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Detects the currently selected item fom the selection wheel using the current angle and highlights it.
+    /// </summary>
+    void HighlightSelection()
+    {
+        int step = (360 / elems);
+        int tmp = elems - (int)(curAngle) / step - 1;// map selection from 0 - (elems -1)
+        tmp = Mod((tmp - selectIndex), elems); // select the desired item clockwise
+        if (tmp == selected)
+        {
+            return;
+        }
+        selected = tmp;
+        Debug.Log("selection: " + selected);
+        Text[] texts = GetComponentsInChildren<Text>();
+        for (int i = 0; i < texts.Length; i++)
+        {
+            if (selected != i)
+            {
+                texts[i].fontStyle = FontStyle.Normal;
+                texts[i].color = Color.gray;
+
+            }
+            else
+            {
+                texts[i].fontStyle = FontStyle.Bold;
+                texts[i].color = Color.black;
+            }
+        }
+    }
+    #endregion
+
+    /// <summary>
+    /// Initialize Wheel: Place each child (text) elem evenly on wheel. 
+    /// </summary>
+    void Start()
+    {
+        Vector2 radius;
+        RectTransform t = GetComponent<RectTransform>();
+        radius.x = t.rect.x * t.localScale.y; //assume square as base
+        radius.y = 0;
+        Component[] children = gameObject.GetComponentsInChildren(typeof(RectTransform));
+        elems = children.Length - 1; //ignore this elem
+        if (selectIndex > elems) selectIndex = selectIndex % elems; //stay within boundaries, not necessarily needed though
+        for (int i = 0; i < children.Length; i++)
+        {
+            Transform transform = (Transform)children[i];
+            if (transform.gameObject != gameObject) //ignore this elem
+            {
+                Debug.Log(children[i].name);
+                // define point on outer circle called offset,start from 0 as 12 o'clock on the circle
+                Vector2 offset = Quaternion.Euler(0, 0, 360 / elems * (i - 1)) * radius; 
+                Debug.Log(offset);
+                //move elem to that position on circle
+                transform.Translate(offset.x, offset.y, 0);
+            }
+        }
+        //counter turn elem to keep text straight
+        if (canvas)
+        {
+            canvas.GetComponent<Canvas>().enabled = false;
+            visible = false;
+        }
+        HighlightSelection();
+    }
+
+    /// <summary>
+    /// Update once per frame
+    /// </summary>
+    void Update()
+    {
+
+        // update the current spin with the mouse wheel
+        double friction;
+        Component[] children;
+        float value = Input.GetAxis("Mouse ScrollWheel");
+        curSpin += value * speed * Time.deltaTime;
+        if (curSpin > -threshold && curSpin < threshold && value == 0)
+        {
+            curSpin = 0;
+            stillVisible = false;
+            StartCoroutine(DisableCanvas());
+        }
+        Debug.Log(value);
+        if (value != 0)
+        {
+            stillVisible = true;
+            if (!visible) EnableCanvas();
+        }
+        curAngle = transform.rotation.eulerAngles.z;
+        friction = curSpin * Time.deltaTime;
+        curSpin -= (float)friction;
+        // finally, do the actual rotation
+        transform.Rotate(Vector3.back * (float)curSpin, Space.World);
+        //counter rotate children
+        children = gameObject.GetComponentsInChildren(typeof(RectTransform));
+        for (int i = 0; i < children.Length; i++)
+        {
+            Transform transform = (Transform)children[i];
+            if (transform.gameObject != gameObject)
+            {
+                transform.Rotate(Vector3.back * (float)(-curSpin), Space.Self);
+            }
+        }
+        HighlightSelection();
+    }
+}
+
