299543e6f8faa88be1399f89290610087e52659b
Joana Pecholt <jopech@gmx.de>
2017-07-28 15:20:28 +0200

    [Michi] added arm_simplified AGAIN changed .sdf so it works with sdf_dom.py

--- Assets/ExternalTools/SDF_reader.py
+++ Assets/ExternalTools/SDF_reader.py
@@ -11,13 +11,29 @@ root = CreateFromDocument(open(sdf_location).read())
 
 robot = root.model[0]
 
+print(len(robot.link))
+
 # create a text file
 localfile = open("Assets//ExternalTools//tempModelSDFs.txt" ,'w')
 
-# remove quotation marks
-localfile.write("Model Name = ")
-# remove quotation marks and insert github link
-localfile.write(robot.name + "\n")
+links = robot.link
+
+#for link in links
+#i = 1
+for i in range(0, len(links)):
+#while i <= len(robot.link)
+	localfile.write("Model Link = ")
+	localfile.write(links[i].name + "\n")
+	
+	localfile.write("Link Pose = ")
+	localfile.write(links[i].pose[0] + "\n")
+	
+	VIS_model = links[i].visual[0]
+	VIS_geometry = VIS_model.geometry[0]
+	#if mesh[0] in VIS_geometry 
+	#VIS_mesh = VIS_geometry.mesh[0]
+	#localfile.write("Visual uri = " + VIS_uri + "\n")
+	#i++
 
 
 localfile.close()
